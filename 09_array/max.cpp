3a40e291a0a5958fadb628d1b9de5081d4f2d5ab
vijaykumar <pawarvijaykumar821@gmail.com>
2026-07-30 09:49:01 +0530

    hello

--- 09_array/max.cpp
+++ 09_array/max.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-#include<limits.h>
+#include<limits.h>//store the range is -2^31 or +2^31
 using namespace std;
 int getmax(int num[],int n){
-  int max=INT_MIN;
+  int max=INT_MIN;//minimum range
   for(int i=0;i<n;i++){
     if(num[i]>max){
       max=num[i];
@@ -21,7 +21,7 @@ int main(){
   for(int i=0;i<size;i++){
     cin>>num[i];
   }
-  cout<<"the maximum number is \n"<<getmax(num,size)<<endl;
+  cout<<"the maximum number is \n"<<getmax(num,size)<<endl;//calling the function 
   
   return 0;
 }
