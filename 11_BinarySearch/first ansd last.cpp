d3510fcd812b37893feaef6ebde80fd31d4cd403
vijaykumar <pawarvijaykumar821@gmail.com>
2026-08-24 02:31:14 +0530

    leedcode

--- 11_BinarySearch/first ansd last.cpp
+++ 11_BinarySearch/first ansd last.cpp
@@ -1,58 +1,65 @@
 #include<iostream>
 using namespace std;
-int binarySearch(int arr[],int key,int n){
+int fisrtOcc(int arr[],int n,int key){
   //start position
   
   int start=0;
   int end=n-1;
+  int ans=-1;
   while(start<=end){
     int mid=(start+end)/2;
-   // return mid;key==arr[mid
-    if(key==arr[mid]){
-    //start=mid+1;
-    return mid;
-    }
-    else if(key>arr[mid]){
+    if(key>arr[mid]){
+      ans= mid;
+      start=mid-1;
+    }else if(key==arr[mid]){
       start=mid+1;
-    }else{
-      end =mid-1;
-    }
-    //mid=(start+end)/2;
-
-  }
     
- //
+    }
+    else if(key<arr[mid]){
+      end=mid-1;
+    }
+    mid=(start+end)/2;
+  }
+  return ans;
+
 }
-int binarySearch(int arr[],int key,int n){
+   
+ //
+
+int lastOcc(int arr[],int n,int key){
+
 int start=0;
 int end=n-1;
-int mid=(start+end)/2;
-while(key==arr[mid]){
+int ans=-1;
+while(start<=end){
+  int mid=(start+end)/2;
     //return mid;
     if(key==arr[mid]){
+      ans =mid;
       start=mid+1;
-    }else{
-      start=mid-1;
+    }else if(key>arr[mid]){//move right side
+      start=mid+1;
     }
-    if(key>arr[mid]){
-      start=mid+1;
-    }else{
-      mid=mid-1;
+    if(key<arr[mid]){//move left side
+      end=mid-1;
     }
   
 
-   // mid=(start+end)/2;
+    mid=(start+end)/2;
 
   }
     
-  return -1;
+  return ans;
 
 }
 
 int main(){
-  int a[5]={4,5,4,3,6};
-  int aindex= binarySearch(a,5,4);
-  cout<<"the first ans last postion of value is\n"<<aindex<<endl;
+  int a[5]={1,2,3,3,5};
+  int aindex1= fisrtOcc(a,5,3);
+  int aindex2= lastOcc(a,5,3);
+
+  cout<<"the first ans last postion of value is 1\n"<<aindex1<<endl;
+  cout<<"the first ans last postion of value is 2\n"<<aindex2<<endl;
 
 
     
