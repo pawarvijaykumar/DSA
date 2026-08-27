536cccef4aaab3512b176639e01a5f827467472c
vijaykumar <pawarvijaykumar821@gmail.com>
2026-08-27 14:46:56 +0530

    panters

--- 12_book alocation problem/tempCodeRunnerFile.cpp
+++ 12_book alocation problem/tempCodeRunnerFile.cpp
@@ -2,38 +2,38 @@
 #include <vector>
 using namespace std;
 
-bool isPossible(vector<int>& books, int students, int maxPages) {
+bool isPossible(vector<int>& boards, int painter, int maxTime) {
 
-    int studentCount = 1;
-    int pages = 0;
+    int painterCount = 1;
+    int time = 0;
 
-    for(int i = 0; i < books.size(); i++) {
+    for(int i = 0; i < boards.size(); i++) {
 
-        if(pages + books[i] <= maxPages) {
-            pages += books[i];
+        if(time + boards[i] <= maxTime) {
+            time += boards[i];
         }
         else {
-            studentCount++;
+            painterCount++;
 
-            if(studentCount > students || books[i] > maxPages) {
+            if(painterCount > painter || boards[i] > maxTime) {
                 return false;
             }
 
-            pages = books[i];
+            time = boards[i];
         }
     }
 
     return true;
 }
 
-int bookAllocation(vector<int>& books, int students) {
+int bookAllocation(vector<int>& boards, int painter) {
 
     int start = 0;
     int end = 0;
 
-    for(int pages : books) {
-        start = max(start, pages);
-        end += pages;
+    for(int time : boards) {
+        start = max(start, time);
+        end += time;
     }
 
     int ans = -1;
@@ -42,7 +42,7 @@ int bookAllocation(vector<int>& books, int students) {
 
         int mid = start + (end - start) / 2;
 
-        if(isPossible(books, students, mid)) {
+        if(isPossible(boards, painter, mid)) {
 
             ans = mid;
             end = mid - 1;
@@ -55,9 +55,9 @@ int bookAllocation(vector<int>& books, int students) {
     return ans;
 }
 int main(){
-  vector<int>books={1,34,2,5,3};
-  int students=2;
-  int ans=bookAllocation( books, students);
+  vector<int>boards={1,34,2,5,3};//output 35 y?-->1+34=35nad 2+5+3=10 35||10 so 35 is choosen
+  int painter=2;
+  int ans=bookAllocation( boards, painter);
   cout<<"the minimum maximum pages \n"<<ans<<endl;
   return 0;
 }
