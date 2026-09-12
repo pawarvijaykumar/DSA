c85c8a8d5d8bc2ec452cd960d7b267e34e1e7b2a
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-13 03:10:14 +0530

    pointer in fun

--- 11_BinarySearch/tempCodeRunnerFile.cpp
+++ 11_BinarySearch/tempCodeRunnerFile.cpp
@@ -1,36 +1,37 @@
 #include<iostream>
 using namespace std;
+int binarySearch(int arr[],int n,int key){
+  int start=0;
+  int end=n-1;
+  int mid=(start+end)/2;
+  while(start<=end){
+    if(arr[mid]==key){//key is ur target value
+      return mid;
 
-long long int binarySearch(int n){
-        //so i long y was i write becuse its range 21^n+1 or 21^n-1
-        int start=0;
-        int end=n;
-        long long int mid=(start+end)/2;
-        long long int ans=-1;
-        while(start<=end){
-            long long int square=mid*mid;
 
-            if(square==n){
-                return mid;
-
-            }else if(square<n){//move reght side
-                start=mid+1;
-                ans=mid;
-            }else{
-                end=mid-1;//otherwise left side
-            }
-            mid=(start+end)/2;
-        }
-        return ans;
     }
+    if(key>arr[mid]){
+      start=mid+1;//to move right 
+    }else{
+      end=mid-1;
+
+    }
+    mid=(start+end)/2;//last value which that print
+    }
+    return -1;//otherwise print -1
+}
+
 int main(){
-  int n;
-  cout<<"enter the suare lement is"<<endl;
-  cin>>n;
-  int tempSol=binarySearch( n);
-  cout<<"the sqaure is \n"<< tempSol<<endl;
-
-
   
+  int a[6]={5,7,7,8,8,10};
+  int aindex= binarySearch(a,6,8);
+  cout<<"the key value is\n"<<aindex<<endl;
+
   return 0;
 }
+
+
+/*time complexicity =0(logn)-->
+you move to find for right or left mid arr n/2,againn/2......upto 
+n/2^k=1,then
+n=2^k---->k=logn this is a time complexcity*/
