31d6933cc80c56d37121d80eae73c76e986833eb
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-14 23:38:08 +0530

    dynamic 2d array

---
+++ 29-lec-dynamic memeory alo/intro.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int row;
+
+  cin>>row;
+
+  int col;
+
+  cin>>col;
+  int **arr=new int *[row];//use 2d array
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      arr[i]=new int [row];
+    }
+  }
+
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      cin>>arr[i][j];
+    }
+  }
+
+
+  cout<<endl;
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+
+  
+  //memeory release
+  for(int i=0;i<row;i++){
+    delete [] arr[i];
+
+  }
+  delete []arr;
+
+  return 0;
+}
