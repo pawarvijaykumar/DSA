eba6b02894f84ac164a440f30c25b8461f608db5
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-15 19:46:40 +0530

    Square macros

--- 30-lec-macro,global,inline fun/macros.cpp
+++ 30-lec-macro,global,inline fun/macros.cpp
@@ -5,12 +5,14 @@ using namespace std;
 //A macro is a name that represents some value or piece of code
 # define PI 3.21//isme ; nahi aata hai 
 # define LIMIT 7
+# define SQUARE (x) (x*x)
 int main(){
   int n=5;
 
   cout<<PI*n*n<<endl;
   cout<<" so my age id "<<LIMIT<<endl;
   printf("locdown will be extend %d \n ",LIMIT);
+  // cout<<SQUARE(5)<<endl;/25
   
   return 0;
 }
