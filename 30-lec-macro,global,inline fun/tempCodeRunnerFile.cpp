b8789da198d3ae4d1037d7dd08759eb73d9b31b6
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-15 21:32:08 +0530

    global variable

---
+++ 30-lec-macro,global,inline fun/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int score =23;
+ void a(int& x){
+  cout<<x<<endl;
+  cout<<"x in<<"<<score<<endl;
+ }
+ void b(int& y){
+  cout<<y<<endl;
+  cout<<"y in" <<score<<endl;
+ }
+int main(){
+  int x;
+  int y;
+  int i=7;
+  cout<<score<<endl;
+  {  // {
+    int h=8;
+    cout<<h<<endl;//is local variable
+  }
+  a(x);
+  b(y);
+
+  return 0;
+}
