6020e4e10c8323b8e9b82377a9de6ba7ed6fe69c
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-22 01:24:48 +0530

    added recursion programs

--- 22_lec-string/tempCodeRunnerFile.cpp
+++ 22_lec-string/tempCodeRunnerFile.cpp
@@ -1,53 +1,26 @@
 #include<iostream>
 using namespace std;
-void reverseString(char name[],int n){
-  
+bool palindrom(int  arr[],int n){
   int start=0;
   int end=n-1;
   while(start<=end){
-    swap(name[start++],name[end--]);
+    if(arr[start++]!=arr[end--]){
+      return 0;
+    }else{
+      // start++;
+      // end--;
+    }
   }
-  
+  return 1;
 
 }
-// int getlength(char name[]){  
-//   int count=0;
-//   for(int i=0;name[i]!='\0';i++){//the arr will be stop upto null come and then after is given automaticaly stop and then we know lentgh of arr
-//     count++;
-
-//   }
-//   return count;
-// }
-
 int main(){
-  char name[20];
-  cout<<"enter your name"<<endl;
-  cin>>name;
-  //int len=getlength(name);
-  //cout<<"the reverse string is "<<len<<endl;
-  reverseString(name,3);
-  cout<<"the name is   "  <<name<<endl;
+  int arr[3]={'a','r','t'};
+  int index=palindrom(arr,3);
+  
+  cout<<"the palindrom is "<<index<<endl;
+  
+
+  
   return 0;
-};
-
-  
-  
-
-  // int getlength(char name[]){
-  // int count=0;
-  // for(int i=0;name[i]!='\0';i++){//the arr will be stop upto null come and then after is given automaticaly stop and then we know lentgh of arr
-  //   count++;
-
-  //}
-  // return count;
-
-// int main(){
-//   char name[20];
-//   cout<<"enter your name"<<endl;
-//   cin>>name;
-//   //name[3]='\0';//is a null chararcter means when i enter mynname vijaybthe give vu-ij becuse index is 3 null character
-//   cout<<"the name is   "  <<name<<endl;
-//   cout<<"the lentgh of my name is "<<getlength(name);
-
-//   return 0;
-// }
+}
