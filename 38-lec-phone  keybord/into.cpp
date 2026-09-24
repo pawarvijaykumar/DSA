f9a593b32092e69e95c8373412e198232b08e1c1
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-24 15:25:41 +0530

    phone keyboard

--- 38-lec-phone  keybord/into.cpp
+++ 38-lec-phone  keybord/into.cpp
@@ -25,7 +25,10 @@ vector<string> letterCombinations(string digits) {
   }
   string output="";
   int index=0;
-  string mapping[10] = {"", "123", "abc", "def", "ghi","jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+
+  //" "
+  string mapping[10] = {" ", " ", "abc", "def", "ghi","jkl", "mno", "pqrs", "tuv", "wxyz"};
   solve(digits,output,index,ans,mapping);
   return ans;
 
