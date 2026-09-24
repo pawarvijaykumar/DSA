75404a416bd5b245a067fc3993f0e77a8ae52959
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-24 22:22:24 +0530

    permutation

---
+++ 37-lec-permutation in str/inStr.cpp
@@ -0,0 +1,16 @@
+/*
+                    abc
+              /      |      \
+             a       b       c
+            / \     / \     / \
+           ab  ac  ba  bc  cb  ca
+           |   |   |   |   |   |
+          abc acb bac bca cba cab
+
+
+          
+What is a permutation
+A permutation means arranging all characters of a string in different possible orders.
+
+  Important formula ->>>>Number of permutations = n!
+*/
